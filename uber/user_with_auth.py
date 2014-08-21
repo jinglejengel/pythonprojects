e9d671618d55168b6dd113ea53fcb90b1ff1b482
Joe Engel <engel.joe@me.com>
2014-08-20 19:21:48 -0500

    Create user_with_auth.py

---
+++ uber/user_with_auth.py
@@ -0,0 +1,32 @@
+from requests_oauthlib import OAuth2Session
+import json
+
+client_id = r''
+client_secret = r''
+redirect_uri = 'https://67.192.246.50'
+
+oauth = OAuth2Session(client_id, redirect_uri=redirect_uri)
+authorization_url, state = oauth.authorization_url(
+        'https://login.uber.com/oauth/authorize')
+
+print 'Please go to %s and authorize access.' % authorization_url
+authorization_response = raw_input('Enter the full callback URL: ')
+
+
+token = oauth.fetch_token(
+        'https://login.uber.com/oauth/token',
+        authorization_response=authorization_response,
+        client_secret=client_secret)
+
+prof = oauth.get('https://api.uber.com/v1/me')
+hist = oauth.get('https://api.uber.com/v1/history')
+
+print "Your profile is: "
+print json.dumps(prof.json(), indent=4)
+
+print
+
+print "Your history is: "
+print json.dumps(hist.json(), indent=4)
+
+print
